e222ca691d69411f801b581176aeb5b904add859
blackfly0 <gtx3DLed>
2020-07-02 14:55:56 +0500

    - Added Camera Support

--- app/src/main/java/com/example/pestclassifier/Constants.java
+++ app/src/main/java/com/example/pestclassifier/Constants.java
@@ -1,10 +1,95 @@
 package com.example.pestclassifier;
 
-import android.Manifest;
-import android.widget.Toast;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.util.Log;
 
-import java.util.List;
+import static android.content.ContentValues.TAG;
 
 public class Constants{
 
+    public static String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } catch (Exception e) {
+            Log.e(TAG, "getRealPathFromURI Exception : " + e.toString());
+            return "";
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    public static String getFileName(Uri uri, MainActivity x) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = x.getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
+    public static Bitmap correctOrientationOfBitmap(Bitmap bitmap, String currentPhotoPath){
+        int orientation = 0;
+        try {
+            ExifInterface ei = new ExifInterface(currentPhotoPath);
+            orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_UNDEFINED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Bitmap rotatedBitmap = null;
+        switch(orientation) {
+
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                rotatedBitmap = rotateImage(bitmap, 90);
+                break;
+
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                rotatedBitmap = rotateImage(bitmap, 180);
+                break;
+
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                rotatedBitmap = rotateImage(bitmap, 270);
+                break;
+
+            case ExifInterface.ORIENTATION_NORMAL:
+            default:
+                rotatedBitmap = bitmap;
+        }
+        return rotatedBitmap;
+    }
+
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
 }
--- app/src/main/java/com/example/pestclassifier/MainActivity.java
+++ app/src/main/java/com/example/pestclassifier/MainActivity.java
@@ -1,9 +1,29 @@
 package com.example.pestclassifier;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.karumi.dexter.Dexter;
@@ -14,14 +34,64 @@ import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.PermissionRequestErrorListener;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import static android.R.layout.simple_spinner_dropdown_item;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private ImageView imageview;
+    private ImageView placeHolderIV;
+    private Button selectImageButton;
+    private Button uploadButton;
+    private Spinner selectModelSpinner;
+
+    private int GALLERY = 1, CAMERA = 2;
+    private boolean imageLoaded = false;
+    boolean isFromGallery;
+    private String currentPhotoName,currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestMultiplePermissions();
+
+        setupView();
+        setupSpinner();
+        setupButtonListeners();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == this.RESULT_CANCELED) {
+            return;
+        }
+        if (requestCode == GALLERY) {
+            imageLoaded = true;
+            placeHolderIV.setImageBitmap(null);
+            if (data != null) {
+                isFromGallery = true;
+                Uri contentURI = data.getData();
+                currentPhotoPath = Constants.getRealPathFromURI(this, contentURI);
+                currentPhotoName = Constants.getFileName(contentURI, this);
+                setPic();
+            }
+
+        } else if (requestCode == CAMERA && resultCode == RESULT_OK) {
+            isFromGallery = false;
+            placeHolderIV.setImageBitmap(null);
+            imageLoaded = true;
+            setPic();
+        }
     }
 
     public void  requestMultiplePermissions(){
@@ -59,4 +129,172 @@ public class MainActivity extends AppCompatActivity {
                 .onSameThread()
                 .check();
     }
+
+    private void setupView(){
+        uploadButton = findViewById(R.id.uploadButton);
+        selectImageButton = findViewById(R.id.selectImageButton);
+        imageview = findViewById(R.id.imageView);
+        placeHolderIV = findViewById(R.id.ivPlaceholder);
+        selectModelSpinner = findViewById(R.id.selectModelSpinner);
+    }
+    private void setupSpinner(){
+        // Spinner Drop down elements
+        List<String> modelNames = new ArrayList<String>();
+        modelNames.add("Rice");
+        modelNames.add("Corn");
+        modelNames.add("Wheat");
+        modelNames.add("Mango");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, modelNames);
+        dataAdapter.setDropDownViewResource(simple_spinner_dropdown_item);
+
+        selectModelSpinner.setAdapter(dataAdapter);
+        selectModelSpinner.setAdapter(
+                new NothingSelectedSpinnerAdapter(
+                        dataAdapter,
+                        R.layout.contact_spinner_row_nothing_selected,
+                        // R.layout.contact_spinner_nothing_selected_dropdown, // Optional
+                        this));
+
+        selectModelSpinner.setPrompt("Select Pretrained Model");
+        selectModelSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0){
+                    String item = parent.getItemAtPosition(position).toString();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        });
+    }
+
+    private void setupButtonListeners(){
+        selectImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPictureDialog();
+            }
+        });
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadButtonTapped();
+            }
+        });
+    }
+
+    private void showPictureDialog(){
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
+        pictureDialog.setTitle("Select Action");
+        String[] pictureDialogItems = {
+                "Select photo from gallery",
+                "Capture photo from camera" };
+        pictureDialog.setItems(pictureDialogItems,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                choosePhotoFromGallary();
+                                break;
+                            case 1:
+                                takePhotoFromCamera();
+                                break;
+                        }
+                    }
+                });
+        pictureDialog.show();
+    }
+    public void choosePhotoFromGallary() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, GALLERY);
+    }
+    private void takePhotoFromCamera() {
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            // Error occurred while creating the File
+            Log.d(TAG, ex.toString());
+        }
+        if (photoFile != null) {
+            Uri photoURI = FileProvider.getUriForFile(this,
+                    "com.example.pestclassifier.fileprovider",
+                    photoFile);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            intent.putExtra("return-data", true);
+            startActivityForResult(intent, CAMERA);
+        }
+    }
+
+    private void uploadButtonTapped(){
+
+        if(!imageLoaded){
+            Toast.makeText(this, "Please upload an image first!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+//        Location location = getLocationWithCheckNetworkAndGPS(getApplicationContext());
+//        if(location == null){
+//            Toast.makeText(this, "Could not get location!", Toast.LENGTH_SHORT).show();
+//        }
+        // Upload the image
+        uploadImage();
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        currentPhotoName = imageFileName;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void uploadImage(){
+
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = imageview.getWidth();
+        int targetH = imageview.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        bitmap = Constants.correctOrientationOfBitmap(bitmap, currentPhotoPath);
+        imageview.setImageBitmap(bitmap);
+    }
+
 }
